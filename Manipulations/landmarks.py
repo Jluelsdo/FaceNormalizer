bf37cb53b42fe2ec25361d701680be333e49c879
Jonas L <jonas.luelsdorf@gmx.net>
2024-08-15 10:01:33 +0200

    adjust landmarks.py

--- Manipulations/landmarks.py
+++ Manipulations/landmarks.py
@@ -3,8 +3,8 @@ import subprocess
 def start_landmark_search(path_sourcedata, landmarks_config, path_landmarks):
     #Todo: save the landmarks in the correct path_landmarks
 
-    command = f"python Deep-MVLM/predict.py --config {landmarks_config} --name {path_sourcedata}"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    command = f"python predict.py --c {landmarks_config} --n {path_sourcedata}"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, cwd="Deep-MVLM")
     process.wait()
 
     return True
