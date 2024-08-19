7508bec4f3fbb58d7fa66a34fbf92c6895e1a88c
Jonas L <jonas.luelsdorf@gmx.net>
2024-08-19 09:54:54 +0200

    Some exception catching within Deep-MVLM call

--- Manipulations/landmarks.py
+++ Manipulations/landmarks.py
@@ -3,8 +3,13 @@ import subprocess
 def start_landmark_search(path_sourcedata, landmarks_config, path_landmarks):
     #Todo: save the landmarks in the correct path_landmarks
 
-    command = f"python predict.py --c {landmarks_config} --n {path_sourcedata}"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, cwd="Deep-MVLM")
-    process.wait()
+    command = f"python Deep-MVLM/predict.py --c {landmarks_config} --n {path_sourcedata}"
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        # Handle the error here
+        print(f"Error: {e}")
+    finally:
+        pass#subprocess.run("pkill -f unix_socket_name", shell=True)
 
     return True
