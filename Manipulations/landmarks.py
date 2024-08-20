6744fe81510d4f3975dd606cb847f92641352a4c
Jonas L <jonas.luelsdorf@gmx.net>
2024-08-20 10:31:41 +0200

    Update start landmarks search, add some todo for later issues, works for now at testing stage

--- Manipulations/landmarks.py
+++ Manipulations/landmarks.py
@@ -1,15 +1,17 @@
 import subprocess
+import os
+import signal
+import time
 
 def start_landmark_search(path_sourcedata, landmarks_config, path_landmarks):
     #Todo: save the landmarks in the correct path_landmarks
+    #Todo: Integrate the landmarks search through the Deep-MVLM library
+    #Todo: Add the search for outside testing (Mulitple Landmarks n paths)
 
     command = f"python Deep-MVLM/predict.py --c {landmarks_config} --n {path_sourcedata}"
-    try:
-        subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        # Handle the error here
-        print(f"Error: {e}")
-    finally:
-        pass#subprocess.run("pkill -f unix_socket_name", shell=True)
-
+    process = subprocess.Popen(command, shell=True)
+    pid = process.pid
+    while(not os.path.exists("Data/Testdata/original/testscan_landmarks.txt")):
+        time.sleep(1)
+    os.kill(pid, signal.SIGTERM)
     return True
