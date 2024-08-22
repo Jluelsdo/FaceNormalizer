5f87d93cc021c5f5f1622624d34764eb5b4e564c
Jonas L <jonas.luelsdorf@gmx.net>
2024-08-22 14:23:19 +0200

    Export cutting logic in a centraliced function within Cutting class, where each module is set up within an order set in the config.json Allow multiple files to run

--- Manipulations/cut.py
+++ Manipulations/cut.py
@@ -1,7 +1,45 @@
 import numpy as np
 from scipy.spatial import ConvexHull, Delaunay
 import trimesh
+import os
 class Cutting:
+
+    def run_cutting(self, path_sourcedata, path_target_cutting, order_cutting, path_target_landmarks, save_intermediate_step=True):
+
+        for file in os.listdir(path_sourcedata):
+            if not file.endswith(".stl"):
+                continue
+
+            file_path = os.path.join(path_sourcedata, file)
+            path_landmarks = os.path.join(path_target_landmarks, file.replace(".stl", "_landmarks.txt"))
+            if not os.path.exists(path_landmarks):
+                print(f"Error: Landmarks file does not exist at {path_landmarks}!")
+                continue
+            landmarks = np.loadtxt(path_landmarks)
+            mesh = trimesh.load_mesh(file_path)
+
+            # Cut the face
+            for cut in order_cutting:
+                if cut == "outline":
+                    self._cutting_outlines_through_hull(file_path, os.path.join(path_target_cutting, file), path_landmarks, save_intermediate_step)
+                elif cut == "upper_face":
+                    try:
+                        cut_vertices_upper = self._cut_upper_face(landmarks, cut_vertices_lower)
+                        cut_mesh = self._simulate_faces(cut_vertices_upper)
+                    except NameError:
+                        cut_vertices_upper = self._cut_upper_face(landmarks, mesh.vertices)
+                elif cut == "lower_face":
+                    try:
+                        cut_vertices_lower = self._cut_lower_face(landmarks, cut_vertices_upper)
+                        cut_mesh = self._simulate_faces(cut_vertices_lower)
+                    except NameError:
+                        cut_vertices_lower = self._cut_lower_face(landmarks, mesh.vertices)
+
+            # Export the cut mesh
+            if save_intermediate_step:
+                cut_mesh.export(os.path.join(path_target_cutting, file.replace(".stl", "_cut.stl")))
+            cut_mesh.export(file_path)
+        return True
 
     def _simulate_faces(self, vertices):
         """Due to the cutting of the face we need to simulate the faces missing. This is done by triangulating the face."""
@@ -9,9 +47,8 @@ class Cutting:
         tri = Delaunay(mesh.vertices[:,:2])
         return trimesh.Trimesh(vertices=vertices, faces=tri.simplices)
 
-    def cutting_outlines_through_hull(self, path_sourcedata, path_targetdata, path_landmarks, cutting_config):
+    def _cutting_outlines_through_hull(self, path_sourcedata, path_targetdata, path_landmarks, save_intermediate_step):
         """Start the cutting of the face."""
-        print("Cutting")
         landmarks = np.loadtxt(path_landmarks)
         hull = ConvexHull(landmarks)
         inflated_points = np.zeros((0,3))
@@ -31,20 +68,13 @@ class Cutting:
                 filtered_vertices.append(vertex)
 
         cut_mesh = self._simulate_faces(filtered_vertices)
+        cut_mesh.export(path_sourcedata)
 
-        cut_mesh.export(path_targetdata)
+        if save_intermediate_step:
+            cut_mesh.export(path_targetdata.replace(".stl", "_cut.stl"))
         return True 
 
-    def cut_upper_lower_face(self, path_sourcedata, path_targetdata, path_landmarks, cutting_config):
-        landmarks = np.loadtxt(path_landmarks)
-        mesh = trimesh.load_mesh(path_sourcedata)
-        cut_vertices_upper = self._cut_upper_face(landmarks, mesh)
-        cut_vertices_lower = self._cut_lower_face(landmarks, cut_vertices_upper)
-        cut_mesh = self._simulate_faces(cut_vertices_lower)
-        cut_mesh.export(path_targetdata)
-        return True
-
-    def _cut_upper_face(self, landmarks, mesh):
+    def _cut_upper_face(self, landmarks, vertices):
         # Cut upper face
         p1 = landmarks[32]
         p2 = landmarks[33]
@@ -55,10 +85,10 @@ class Cutting:
         normal /= np.linalg.norm(normal)
         d = -np.dot(normal, p1)
         # Cut the model with the plane using a vectorized operation
-        condition = np.dot(mesh.vertices, normal) + d >= 0
-        return mesh.vertices[condition]
+        condition = np.dot(vertices, normal) + d >= 0
+        return vertices[condition]
 
-    def _cut_lower_face(self, landmarks, cut_vertices_upper):
+    def _cut_lower_face(self, landmarks, vertices):
         p1 = landmarks[62]
         p2 = landmarks[67]
         p3 = landmarks[72]
@@ -69,8 +99,5 @@ class Cutting:
         d = -np.dot(normal, p1)
 
         # Cut the model with the plane using a vectorized operation
-        condition = np.dot(cut_vertices_upper, normal) + d >= 0
-        return cut_vertices_upper[condition]
-
-cutting = Cutting()
-cutting.cut_upper_lower_face("Data/Testdata/original/testscan.stl", "Data/Testdata/original/testscan_cut.stl", "Data/Testdata/original/testscan_landmarks.txt", None)
+        condition = np.dot(vertices, normal) + d >= 0
+        return vertices[condition]
