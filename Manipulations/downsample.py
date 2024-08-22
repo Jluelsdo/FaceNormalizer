4c1460c55dff2478c314c82078787d9b28b68be3
Jonas L <jonas.luelsdorf@gmx.net>
2024-08-22 14:24:18 +0200

    Update downsample.pyx to work with multiple files

--- Manipulations/downsample.py
+++ Manipulations/downsample.py
@@ -1,16 +1,28 @@
 import trimesh
+import os
 
-def downsample(input_file, output_file, num_samples):
+def downsample(input_path, output_path, num_samples, save_intermediate_step=True):
     """
     Load the mesh and downsample it to the number of samples set in the config.json file.
     simplify_quadratic_decimation is a wrapper for open3d.geometry.TriangleMesh
     """
-    # Load the mesh using trimesh
-    mesh = trimesh.load_mesh(input_file)
-    
-    # Simplify the mesh using trimesh's simplify_quadratic_decimation
-    simplified_mesh = mesh.simplify_quadratic_decimation(num_samples)
-    
-    # Save the simplified mesh as an STL file
-    simplified_mesh.export(output_file)
+    # Iterate through all files within input_path
+    for file_name in os.listdir(input_path):
+        # Construct the full file path
+        file_path = os.path.join(input_path, file_name)
+        
+        # Check if the file is a mesh file
+        if not file_name.endswith('.stl'):
+            continue
+        # Load the mesh using trimesh
+        mesh = trimesh.load_mesh(file_path)
+        
+        # Simplify the mesh using trimesh's simplify_quadratic_decimation
+        simplified_mesh = mesh.simplify_quadratic_decimation(num_samples)
+        
+        # Save the simplified mesh as an STL file
+        simplified_mesh.export(file_path)
+        if save_intermediate_step:
+            file_output_path = os.path.join(output_path, file_name)
+            simplified_mesh.export(file_output_path.replace(".stl", "_simplified.stl"))
     return True
