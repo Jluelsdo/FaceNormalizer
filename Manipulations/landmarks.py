93aad884f428dcf588b8acd274a87dea51ce3c4a
Jonas Lülsdorf <jonas.luelsdorf@th-koeln.de>
2024-09-16 16:31:24 +0200

    Streamline order of executing to work on one file after another

--- Manipulations/landmarks.py
+++ Manipulations/landmarks.py
@@ -3,27 +3,20 @@ import os
 import signal
 import time
 
-def start_landmark_search(path_sourcedata, landmarks_config, path_landmarks):
+def start_landmark_search(path_file, landmarks_config, path_landmarks):
     """Start the landmark search for the face."""
     #Todo: Integrate the landmarks search through the Deep-MVLM library
-    file_list = []
-    for root, dirs, files in os.walk(path_sourcedata):
-        for file in files:
-            if file.endswith(".stl"):
-                file_list.append(os.path.join(root, file))
-    for file_path in file_list:
-        command = f"python Deep-MVLM/predict.py --c {landmarks_config} --n {file_path}"
-        process = subprocess.Popen(command, shell=True)
-        pid = process.pid
-        landmarks_name = file_path.split("/")[-1].replace(".stl", "_landmarks.txt")
-        path_file_landmarks = path_sourcedata + "/" + landmarks_name
-        while(not os.path.exists(path_file_landmarks)):
-            time.sleep(1)
-        os.rename(path_file_landmarks, path_landmarks+"/"+landmarks_name)
-        os.kill(pid, signal.SIGTERM)
+    path_sourcedata = os.path.dirname(path_file)
+    command = f"python Deep-MVLM/predict.py --c {landmarks_config} --n {path_file}"
+    process = subprocess.Popen(command, shell=True)
+    pid = process.pid
+    landmarks_name = path_file.split("/")[-1].replace(".stl", "_landmarks.txt")
+    path_file_landmarks = path_sourcedata + "/" + landmarks_name
+    while(not os.path.exists(path_file_landmarks)):
+        time.sleep(1)
+    os.rename(path_file_landmarks, path_landmarks+"/"+landmarks_name)
+    os.kill(pid, signal.SIGTERM)
 
-
-    # Remove files ending with .vtk from path_sourcedata
     for root, dirs, files in os.walk(path_sourcedata):
         for file in files:
             if file.endswith(".vtk"):
--- normalize.py
+++ normalize.py
@@ -2,6 +2,8 @@ import json
 from Manipulations.landmarks import start_landmark_search
 from Manipulations.verticeNumber import NormalizeNumberOfVertices
 from Manipulations.cut import Cutting
+import os
+import csv
 class FaceNormalizer():
 
     def __init__(self):
@@ -25,41 +27,56 @@ class FaceNormalizer():
             self.path_target_cutting = config["DataSource"]["path_target_cutting"]
             self.path_target_normalize_number_of_vertices = config["DataSource"]["path_target_normalize_number_of_vertices"]
 
+            self.path_execution_list = config["DataSource"]["path_execution_list"]
+            self.path_log = config["DataSource"]["path_log"]
+
 
     def run_normalization(self):
         """Run the normalization of the face. Logic is described within the Readme.md ManipulationOrder."""
         print(f"Order of manipulation: {self.order_of_manipulation}")
+        file_list = []
+        for root, dirs, files in os.walk(self.path_sourcedata):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.isfile(file_path):
+                    file_list.append(file_path)
+        total_files = len(file_list)
+        processed_files = 0
+        for file_name in file_list:
+            processed_files += 1
+            percentage = (processed_files / total_files) * 100
+            print(f"Processing {file_name} ({percentage:.2f}%)")
+            # Rule 1 Run all Manipulations which are not dependent on other manipulations
+            landmarks_detected = False
+            if "Landmarks" in self.order_of_manipulation:
+                self._run_landmarks(file_name)
+                landmarks_detected = True
+            if "Determine number of vertices" in self.order_of_manipulation:
+                self._run_determine_number_of_vertices()
+            if "Determine orientation" in self.order_of_manipulation and landmarks_detected:
+                self._run_determine_orientation()
+            else:
+                print("Determine orientation is dependent on landmarks. Please add Landmarks to config.json.")
 
-        # Rule 1 Run all Manipulations which are not dependent on other manipulations
-        landmarks_detected = False
-        if "Landmarks" in self.order_of_manipulation:
-            self._run_landmarks()
-            landmarks_detected = True
-        if "Determine number of vertices" in self.order_of_manipulation:
-            self._run_determine_number_of_vertices()
-        if "Determine orientation" in self.order_of_manipulation and landmarks_detected:
-            self._run_determine_orientation()
-        else:
-            print("Determine orientation is dependent on landmarks. Please add Landmarks to config.json.")
+            for manipulation in self.order_of_manipulation:
+                if manipulation == "Rescale size":
+                    self._run_rescale()
+                elif manipulation == "Cutting":
+                    self._run_cutting(file_name)
+                elif manipulation == "Rotate face":
+                    self._run_rotate()
+                elif manipulation == "Translate face":
+                    self._run_translate()
+                    self._run_landmarks(file_name)
+                elif manipulation == "Normalize number of vertices":
+                    self._run_normalize_number_of_vertices()
+                    self._run_landmarks(file_name)
 
-        for manipulation in self.order_of_manipulation:
-            if manipulation == "Rescale size":
-                self._run_rescale()
-            elif manipulation == "Cutting":
-                self._run_cutting()
-            elif manipulation == "Rotate face":
-                self._run_rotate()
-            elif manipulation == "Translate face":
-                self._run_translate()
-                self._run_landmarks()
-            elif manipulation == "Normalize number of vertices":
-                self._run_normalize_number_of_vertices()
-                self._run_landmarks()
+    
 
-
-    def _run_landmarks(self):
+    def _run_landmarks(self, path_file):
         """Run the normalization of the face using landmarks."""
-        start_landmark_search(self.path_sourcedata, self.landmarks_config, self.path_target_landmarks)
+        start_landmark_search(path_file, self.landmarks_config, self.path_target_landmarks)
         print("Landmarks done")
         return True
 
@@ -67,10 +84,10 @@ class FaceNormalizer():
         """Run the normalization of the face using rescaling."""
         print("Rescale")
 
-    def _run_cutting(self):
+    def _run_cutting(self, path_file):
         """Run the normalization of the face using cutting."""
         cutting = Cutting()
-        cutting.run_cutting(self.path_sourcedata, self.path_target_cutting, self.cutting_config["OrderCutting"], self.path_target_landmarks, self.cutting_config["FaceInflation"], self.cutting_config["SaveIntermediateSteps"])
+        cutting.run_cutting(path_file, self.path_target_cutting, self.cutting_config["OrderCutting"], self.path_target_landmarks, self.cutting_config["FaceInflation"], self.cutting_config["SaveIntermediateSteps"])
         print("Cutting done")
         return True
 
@@ -82,10 +99,10 @@ class FaceNormalizer():
         """Run the normalization of the face using translation."""
         print("Translate")
 
-    def _run_normalize_number_of_vertices(self):
+    def _run_normalize_number_of_vertices(self, path_file):
         """Run the normalization of the face using the normalization of the number of vertices."""
         normalizeNumberOfVertices = NormalizeNumberOfVertices()
-        normalizeNumberOfVertices.normalize(self.path_sourcedata, self.path_target_normalize_number_of_vertices, self.normalize_number_of_vertices["Number of Vertices"], self.normalize_number_of_vertices["SaveIntermediateSteps"])
+        normalizeNumberOfVertices.normalize(path_file, self.path_target_normalize_number_of_vertices, self.normalize_number_of_vertices["Number of Vertices"], self.normalize_number_of_vertices["SaveIntermediateSteps"])
         print("Normalize number of vertices")
 
     def _run_determine_number_of_vertices(self):
