842f00db0a1a7295c4a7ead42e12b3f5e9d8d2a6
Jonas L <jonas.luelsdorf@gmx.net>
2025-01-30 14:03:21 +0100

    Update cut file

--- Manipulations/cut.py
+++ Manipulations/cut.py
@@ -4,36 +4,46 @@ import trimesh
 import os
 class Cutting:
 
-    def run_cutting(self, file_path, path_target_cutting, order_cutting, path_target_landmarks, face_inflation=1.5, save_intermediate_step=True):
+    def run_cutting(self, file_path, path_target_cutting, order_cutting, path_target_landmarks, face_inflation=2, save_intermediate_step=True, file_type=".stl"):
         file_name = file_path.split("/")[-1]
-        path_landmarks = os.path.join(path_target_landmarks, file_name.replace(".stl", "_landmarks.txt"))
+
+        path_landmarks = os.path.join(path_target_landmarks, file_name.replace(file_type, "_landmarks.txt"))
         if not os.path.exists(path_landmarks):
             print(f"Error: Landmarks file does not exist at {path_landmarks}!")
         landmarks = np.loadtxt(path_landmarks)
-        mesh = trimesh.load_mesh(file_path)
-
-        # Cut the face
+        cut_mesh = trimesh.load_mesh(file_path)
+        print(f"Face inflation{face_inflation}")
         for cut in order_cutting:
             if cut == "outline":
-                self._cutting_outlines_through_hull(file_path, os.path.join(path_target_cutting, file_name), path_landmarks, face_inflation=face_inflation, save_intermediate_step=save_intermediate_step)
+                cut_mesh = self._cutting_outlines_through_hull(landmarks,cut_mesh, face_inflation=2.0, save_intermediate_step=save_intermediate_step)
             elif cut == "mouth":
-                self._cut_out_mouth(file_path, os.path.join(path_target_cutting, file_name), path_landmarks, face_inflation=face_inflation, save_intermediate_step=save_intermediate_step)
+                cut_mesh = self._cut_out_mouth(landmarks,cut_mesh,face_inflation=2.0)
             elif cut == "upper_face":
                 try:
-                    cut_vertices_upper = self._cut_upper_face(landmarks, cut_vertices_lower)
-                    cut_mesh = self._simulate_faces(cut_vertices_upper)
+                    cut_vertices = self._cut_upper_face(landmarks, cut_vertices)
+                    cut_mesh = self._simulate_faces(cut_vertices)
                 except NameError:
-                    cut_vertices_upper = self._cut_upper_face(landmarks, mesh.vertices)
+                    mesh = trimesh.load_mesh(file_path)
+                    cut_vertices = self._cut_upper_face(landmarks, mesh.vertices)
             elif cut == "lower_face":
                 try:
-                    cut_vertices_lower = self._cut_lower_face(landmarks, cut_vertices_upper)
-                    cut_mesh = self._simulate_faces(cut_vertices_lower)
+                    cut_vertices = self._cut_lower_face(landmarks, cut_vertices)
+                    cut_mesh = self._simulate_faces(cut_vertices)
                 except NameError:
-                    cut_vertices_lower = self._cut_lower_face(landmarks, mesh.vertices)
+                    mesh = trimesh.load_mesh(file_path)
+                    cut_vertices = self._cut_lower_face(landmarks, mesh.vertices)
+            """elif cut == "back_face":
+                try:
+                    cut_vertices = self._cut_back_face(landmarks, cut_vertices)
+                    cut_mesh = self._simulate_faces(cut_vertices)
+                except NameError:
+                    mesh = trimesh.load_mesh(file_path)
+                    cut_vertices = self._cut_back_face(landmarks, mesh.vertices)
+            """
 
         # Export the cut mesh
         if save_intermediate_step:
-            cut_mesh.export(os.path.join(path_target_cutting, file_name.replace(".stl", "_cut.stl")))
+            cut_mesh.export(os.path.join(path_target_cutting, file_name.replace(file_type, "_cut"+file_type)))
         cut_mesh.export(file_path)
         return True
 
@@ -43,19 +53,17 @@ class Cutting:
         tri = Delaunay(mesh.vertices[:,:2])
         return trimesh.Trimesh(vertices=vertices, faces=tri.simplices)
 
-    def _cutting_outlines_through_hull(self, path_sourcedata, path_targetdata, path_landmarks, face_inflation=1.5, save_intermediate_step=True):
+    def _cutting_outlines_through_hull(self, landmarks, mesh, face_inflation=2, save_intermediate_step=True):
         """Start the cutting of the face."""
-        landmarks = np.loadtxt(path_landmarks)
         hull = ConvexHull(landmarks)
         inflated_points = np.zeros((0,3))
         center_hull = np.mean(hull.points, axis=0)
 
         for point in hull.points:
             inflated_point = center_hull + face_inflation *(point-center_hull)
-            q = np.vstack((inflated_points, inflated_point))
+            inflated_points = np.vstack((inflated_points, inflated_point))
 
         inflated_hull = ConvexHull(inflated_points)
-        mesh = trimesh.load_mesh(path_sourcedata)
 
         verticices = mesh.vertices
         filtered_vertices = []
@@ -64,16 +72,10 @@ class Cutting:
                 filtered_vertices.append(vertex)
 
         cut_mesh = self._simulate_faces(filtered_vertices)
-        cut_mesh.export(path_sourcedata)
 
-        if save_intermediate_step:
-            cut_mesh.export(path_targetdata.replace(".stl", "_cut.stl"))
-        return True 
+        return cut_mesh 
 
-    def _cut_out_mouth(self, path_sourcedata, path_targetdata, path_landmarks, face_inflation=1.7, save_intermediate_step=True):
-        
-        # Load landmarks
-        landmarks = np.loadtxt(path_landmarks)
+    def _cut_out_mouth(self, landmarks, mesh, face_inflation=2):
         #Choose landmarks surrounding the mouth
         selected_landmarks = landmarks[46:55]
 
@@ -88,8 +90,6 @@ class Cutting:
 
         inflated_hull = ConvexHull(inflated_points)
 
-        # Load stl scan
-        mesh = trimesh.load_mesh(path_sourcedata)
         verticices = mesh.vertices
 
         # Decide if the vertex is inside the inflated hull only keep the once outside
@@ -99,36 +99,77 @@ class Cutting:
                 filtered_vertices.append(vertex)
 
         cut_mesh = self._simulate_faces(filtered_vertices)
-        cut_mesh.export(path_sourcedata)
 
-        if save_intermediate_step:
-            cut_mesh.export(path_targetdata.replace(".stl", "_cut.stl"))
-        return True
+        return cut_mesh
 
     def _cut_upper_face(self, landmarks, vertices):
-        # Cut upper face
-        p1 = landmarks[32]
-        p2 = landmarks[33]
-        p3 = (landmarks[34] + landmarks[44]) / 2
+        p1 = landmarks[41]
+        p2 = landmarks[37]
+        p3 = landmarks[39]
+
+        # Calculate the normal of the plane
         v1 = p2 - p1
         v2 = p3 - p1
         normal = np.cross(v1, v2)
         normal /= np.linalg.norm(normal)
         d = -np.dot(normal, p1)
+
+        # Move the plane to the point 
+        p4 = (landmarks[44]+landmarks[34])/2
+        d = -np.dot(normal, p4)
+
         # Cut the model with the plane using a vectorized operation
-        condition = np.dot(vertices, normal) + d >= 0
+        point_position = np.dot(landmarks[49], normal) + d
+        if point_position >= 0:
+            condition = np.dot(vertices, normal) + d >= 0
+        else:
+            condition = np.dot(vertices, normal) + d <= 0
         return vertices[condition]
 
+    
+
     def _cut_lower_face(self, landmarks, vertices):
-        p1 = landmarks[62]
-        p2 = landmarks[67]
-        p3 = landmarks[72]
+        p1 = landmarks[41]
+        p2 = landmarks[37]
+        p3 = landmarks[39]
+
+        # Calculate the normal of the plane
         v1 = p2 - p1
         v2 = p3 - p1
         normal = np.cross(v1, v2)
         normal /= np.linalg.norm(normal)
         d = -np.dot(normal, p1)
 
+        # Move the plane down to the point 67
+        p67 = landmarks[67]
+        d = -np.dot(normal, p67)
+
         # Cut the model with the plane using a vectorized operation
-        condition = np.dot(vertices, normal) + d >= 0
+        point_position = np.dot(landmarks[45], normal) + d
+        if point_position >= 0:
+            condition = np.dot(vertices, normal) + d >= 0
+        else:
+            condition = np.dot(vertices, normal) + d <= 0
+        return vertices[condition]
+    
+    def _cut_back_face(self, landmarks, vertices):
+        # Find the three landmarks with the smallest y coordinate (furthest back)
+        back_landmarks = landmarks[np.argsort(landmarks[:, 1])[:3]]
+        # Define the plane using these three points
+        p1, p2, p3 = back_landmarks
+        v1 = p2 - p1
+        v2 = p3 - p1
+        normal = np.cross(v1, v2)
+        normal /= np.linalg.norm(normal)
+        d = -np.dot(normal, p1)
+        # Make the plane parallel to the y-axis by setting the y component of the normal to 0
+        normal[1] = 0
+        normal /= np.linalg.norm(normal)
+        # Cut the model with the plane using a vectorized operation
+        # Check if the point landmark[45] is in front or behind the plane
+        point_position = np.dot(landmarks[45], normal) + d
+        if point_position >= 0:
+            condition = np.dot(vertices, normal) + d >= 0
+        else:
+            condition = np.dot(vertices, normal) + d <= 0
         return vertices[condition]
